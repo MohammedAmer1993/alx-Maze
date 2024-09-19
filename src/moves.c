9af1a93eb4e3e1461ab6f54157492683ba591067
Mohammed Amer <loard.amer.2010@gmail.com>
2024-09-20 02:28:19 +0300

    edited moves.c heavily, I found out that the cos and sin functions in math lib handles the quarter which the vector is in, so that I didn't need to make any checks on the angle, I'm noticing these four functions are the same with different parameters, let's see about it

--- src/moves.c
+++ src/moves.c
@@ -30,38 +30,12 @@ void moveForward(double *angle, int mov)
 	double xValue = 0;
 	double yValue = 0;
 
-	if (*angle >= 0 && *angle < (PI / 2))
-	{
-		xValue = (mov * cos((*angle)));
-		yValue = (mov * sin((*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;  /* int dosn't have dicimal BUG */
-	}
-	else if ((*angle) >= (PI / 2) && (*angle) < PI)
-	{
-		xValue = -(mov * cos(PI - (*angle)));
-		yValue = (mov * sin(PI - (*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
-	else if ((*angle) >= PI && (*angle) < ((3 * PI) / 2))
-	{
-		xValue = -(mov * sin(((3 * PI) / 2) - (*angle)));
-		yValue = -(mov * cos(((3 * PI) / 2) - (*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
-	else if (((*angle) >= ((3 * PI) / 2)) && (*angle) < (2 * PI))
-	{
-		xValue = (mov * cos(2 * PI - (*angle)));
-		yValue = -(mov * sin(2 * PI - (*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
+	xValue = mov * cos((*angle));
+	yValue = mov * sin((*angle));
+	setAddedValueAfterColl(&xValue, &yValue, current);
+	sprite.x += xValue;
+	sprite.y += yValue;  /* int dosn't have dicimal BUG */
+
 }
 
 /**
@@ -76,38 +50,12 @@ void moveBackward(double *angle, int mov)
 	double xValue = 0;
 	double yValue = 0;
 
-	if (*angle >= 0 && *angle < (PI / 2))
-	{
-		xValue = -(mov * cos((*angle)));
-		yValue = -(mov * sin((*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
-	else if ((*angle) >= (PI / 2) && (*angle) < PI)
-	{
-		xValue = (mov * cos(PI - (*angle)));
-		yValue = -(mov * sin(PI - (*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
-	else if ((*angle) >= PI && (*angle) < ((3 * PI) / 2))
-	{
-		xValue = (mov * sin(((3 * PI) / 2) - (*angle)));
-		yValue = (mov * cos(((3 * PI) / 2) - (*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
-	else if (((*angle) >= ((3 * PI) / 2)) && (*angle) < (2 * PI))
-	{
-		xValue = -(mov * cos(2 * PI - (*angle)));
-		yValue = (mov * sin(2 * PI - (*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
+	xValue = -(mov * cos((*angle)));
+	yValue = -(mov * sin((*angle)));
+	setAddedValueAfterColl(&xValue, &yValue, current);
+	sprite.x += xValue;
+	sprite.y += yValue;
+
 }
 
 /**
@@ -122,38 +70,11 @@ void moveLeft(double *angle, int mov)
 	double xValue = 0;
 	double yValue = 0;
 
-	if (*angle >= 0 && *angle < (PI / 2))
-	{
-		xValue = -(mov * cos(PI - (*angle)));
-		yValue = -(mov * sin(PI - (*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
-	else if ((*angle) >= (PI / 2) && (*angle) < PI)
-	{
-		xValue = -(mov * cos((*angle)));
-		yValue = (mov * sin((*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
-	else if ((*angle) >= PI && (*angle) < ((3 * PI) / 2))
-	{
-		xValue = (mov * cos(2 * PI - (*angle)));
-		yValue = (mov * sin(2 * PI - (*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
-	else if (((*angle) >= ((3 * PI) / 2)) && (*angle) < (2 * PI))
-	{
-		xValue = (mov * sin(((3 * PI) / 2) - (*angle)));
-		yValue = -(mov * cos(((3 * PI) / 2) - (*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
+	xValue = mov * cos((*angle - (PI / 2)));
+	yValue = mov * sin((*angle - (PI / 2)));
+	setAddedValueAfterColl(&xValue, &yValue, current);
+	sprite.x += xValue;
+	sprite.y += yValue;
 }
 
 /**
@@ -169,36 +90,9 @@ void moveRight(double *angle, int mov)
 	double xValue = 0;
 	double yValue = 0;
 
-	if (*angle >= 0 && *angle < (PI / 2))
-	{
-		xValue = (mov * cos(PI - (*angle)));
-		yValue = -(mov * cos(PI - (*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
-	else if ((*angle) >= (PI / 2) && (*angle) < PI)
-	{
-		xValue = -(mov * sin(((3 * PI) / 2) - (*angle)));
-		yValue = (mov * cos(((3 * PI) / 2) - (*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
-	else if ((*angle) >= PI && (*angle) < ((3 * PI) / 2))
-	{
-		xValue = -(mov * cos(2 * PI - (*angle)));
-		yValue = -(mov * sin(2 * PI - (*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
-	else if (((*angle) >= ((3 * PI) / 2)) && (*angle) < (2 * PI))
-	{
-		xValue = (mov * cos((*angle)));
-		yValue = -(mov * sin((*angle)));
-		setAddedValueAfterColl(&xValue, &yValue, current);
-		sprite.x += xValue;
-		sprite.y += yValue;
-	}
+	xValue = mov * (cos(*angle + (PI / 2)));
+	yValue = mov * (sin(*angle + (PI / 2)));
+	setAddedValueAfterColl(&xValue, &yValue, current);
+	sprite.x += xValue;
+	sprite.y += yValue;
 }
